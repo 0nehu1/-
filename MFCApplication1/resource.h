80ba5be1d38a468d60d0a7967094255a8b2559d7
ONEHuI <onejun0320@gmail.com>
2021-11-11 15:32:05 +0900

    초기 커밋.

---
+++ MFCApplication1/resource.h
@@ -0,0 +1,34 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCApplication1.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCAPPLICATION1_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     131
+#define IDB_BITMAP2                     132
+#define IDB_BITMAP3                     133
+#define IDB_BITMAP4                     134
+#define IDB_BITMAP5                     135
+#define IDB_BITMAP6                     144
+#define IDI_ICON1                       152
+#define IDI_ICON2                       153
+#define IDI_ICON3                       154
+#define IDI_ICON4                       155
+#define IDI_ICON5                       156
+#define IDI_ICON6                       157
+#define IDI_ICON7                       158
+#define IDC_LIST1                       1002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        159
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
